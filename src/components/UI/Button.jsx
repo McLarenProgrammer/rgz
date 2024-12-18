04527dffd390217573cc5abd77d6f6745abe36a6
McLarenProgrammer <mtt.programmer@gmail.com>
2024-12-19 00:16:57 +0500

    Докрутил анимацию для кнопки

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,38 +1,42 @@
 import classes from './styles/Button.module.css'
-import { createElement } from 'react';
+import { useState } from 'react';
 
 function Button({ children, clickAction }) {
+    const [waves, setWaves] = useState([]);
+
     function animatedClick(e) {
-        const size = Math.max(e.target.offsetWidth, e.target.offsetHeight);
-        const x = size / 2;
-        const y = size / 2;
-        let wave = createElement(
-            'span',
-            {
-                className: 'wave',
-                style: {
-                    width: size,
-                    height: size,
-                    top: y,
-                    left: x
-                }
+        const size = Math.max(e.currentTarget.offsetWidth, e.currentTarget.offsetHeight);
+        const x = e.currentTarget.offsetWidth / 2 - size / 2;
+        const y = e.currentTarget.offsetHeight / 2 - size / 2;
+
+        const newWave = {
+            id: Date.now(),
+            style: {
+                width: size,
+                height: size,
+                top: y,
+                left: x
             }
-        );
-        e.target.
-    }
+        };
+        setWaves((prevWaves) => [...prevWaves, newWave]);
+
+        setTimeout(() => {
+            setWaves((prevWaves) => prevWaves.filter((wave) => wave.id !== newWave.id));
+        }, 500);
+    };
 
     return (
-        <>
-            <button
-                className={classes.button}
-                onClick={(e) => {
-                    animatedClick(e);
-                    clickAction();
-                }}
-            >
-                {children}
-            </button>
-        </>
+        <button
+            onClick={(e) => {
+                animatedClick(e);
+                clickAction();
+            }}
+        >
+            {children}
+            {waves.map((wave) => (
+                <span key={wave.id} className={classes.wave} style={wave.style} />
+            ))}
+        </button>
     )
 }
 
