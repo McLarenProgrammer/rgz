055ab9532efb84bdb602bee065624f407c312f8b
McLarenProgrammer <mtt.programmer@gmail.com>
2024-12-27 14:58:22 +0500

    Дальнейшая работа над текстовым инпутом

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -4,12 +4,16 @@ import classes from './styles/Input.module.css';
 function Input({ children, inputID = "default", iconSrc = "icons/person.svg", placeholder = "Введите значение", hasIcon = false }) {
     const input = useRef(null);
 
+    function animatedPlaceholder(e) {
+        
+    }
+
     return (
         <>
-            <label className={classes['input-text-label']} htmlFor={inputID}>{children}</label>
             <div className={classes["input-text-container"]}>
                 {hasIcon ? <img className={classes['input-text-icon']} src={iconSrc} onClick={() => input.current.focus() } /> : ''}
-                <input ref={input} className={[ classes['input-text'], hasIcon ? classes['with-icon'] : '' ].join(' ')} type="text" id={inputID} placeholder={placeholder} />
+                <input ref={input} className={[ classes['input-text'], hasIcon ? classes['with-icon'] : '' ].join(' ')} type="text" id={inputID}/>
+                <div className={[classes['input-text-placeholder'], hasIcon ? classes['placeholder-with-icon'] : '' ].join(' ')}>{children}</div>
             </div>
         </>
     )
