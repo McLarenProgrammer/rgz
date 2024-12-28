4cb2ff053b65e3a0df9b77b954be08877f6e50c0
McLarenProgrammer <mtt.programmer@gmail.com>
2024-12-28 15:57:08 +0500

    Делаю анимированный плейсхолдер для инпута

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,19 +1,46 @@
 import { useRef } from 'react';
 import classes from './styles/Input.module.css';
 
-function Input({ children, inputID = "default", iconSrc = "icons/person.svg", placeholder = "Введите значение", hasIcon = false }) {
+function Input({ children, inputID = "default", iconSrc = "icons/person.svg", hasIcon = false }) {
     const input = useRef(null);
+    const placeholder = useRef(null);
 
-    function animatedPlaceholder(e) {
-        
+    function animatedPlaceholder() {
+        if (document.activeElement === input.current) {
+            placeholder.current.classList.add(classes['input-text-placeholder-up']);
+            return;
+        }
+        placeholder.current.classList.remove(classes['input-text-placeholder-up']);
     }
 
     return (
         <>
             <div className={classes["input-text-container"]}>
                 {hasIcon ? <img className={classes['input-text-icon']} src={iconSrc} onClick={() => input.current.focus() } /> : ''}
-                <input ref={input} className={[ classes['input-text'], hasIcon ? classes['with-icon'] : '' ].join(' ')} type="text" id={inputID}/>
-                <div className={[classes['input-text-placeholder'], hasIcon ? classes['placeholder-with-icon'] : '' ].join(' ')}>{children}</div>
+                <input
+                    ref={input}
+                    className={
+                        [
+                            classes['input-text'],
+                            hasIcon ? classes['with-icon'] : '',
+                        ].join(' ')
+                    }
+                    type="text"
+                    id={inputID}
+                    onFocus={animatedPlaceholder}
+                    onBlur={animatedPlaceholder}
+                />
+                <div
+                    ref={placeholder}
+                    className={
+                        [
+                            classes['input-text-placeholder'],
+                            hasIcon ? classes['placeholder-with-icon'] : '',
+                        ].join(' ')
+                    }
+                >
+                    {children}
+                </div>
             </div>
         </>
     )
