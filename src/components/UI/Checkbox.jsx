8a3567a2a4f67fae3bc71edd19279820d270181f
McLarenProgrammer <mtt.programmer@gmail.com>
2025-05-08 18:44:23 +0500

    Мелкие правки Исправил поведение чекбокса. Другие мелкие правки

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,29 +1,43 @@
-import './styles/Checkbox.css'
+import { useState } from "react";
+import "./styles/Checkbox.css";
 
 function Checkbox({ children, inputID = "default" }) {
+    const [checked, setChecked] = useState(false);
+
+    function changeChecked() {
+        setChecked((prev) => !prev);
+    }
+
     return (
-      <div className={classes.checkbox}>
-        <input type="checkbox" id={inputID} />
-        <label htmlFor={inputID} className="checkbox-label">
-          <span className="checkbox-box">
-            <svg
-              className="checkbox-tick"
-              viewBox="0 0 107 154"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid meet"
-            >
-              <path
-                d="M20 70C30 85 37 90 51 120C53 125 58 125 60 120C73 80 83 50 102 20"
-                stroke="#0053A4"
-                strokeWidth="28"
-                strokeLinecap="round"
-              />
-            </svg>
-          </span>
-          <span className="checkbox-label-text">{children}</span>
-        </label>
-      </div>
+        <div className="checkbox">
+            <input
+                type="checkbox"
+                id={inputID}
+                checked={checked}
+                onChange={(e) => setChecked(e.target.checked)}
+            />
+            <div className="checkbox-label">
+                <span className="checkbox-box" onClick={changeChecked}>
+                    <svg
+                        className="checkbox-tick"
+                        viewBox="0 0 107 154"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        preserveAspectRatio="xMidYMid meet"
+                    >
+                        <path
+                            d="M20 70C30 85 37 90 51 120C53 125 58 125 60 120C73 80 83 50 102 20"
+                            stroke="#0053A4"
+                            strokeWidth="28"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                </span>
+                <label htmlFor={inputID} className="checkbox-label-text">
+                    {children}
+                </label>
+            </div>
+        </div>
     );
 }
 
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -2,52 +2,61 @@ import { useRef } from "react";
 import "./styles/Input.css";
 
 function Input({
-  children,
-  inputID = "default",
-  iconSrc = "icons/person.svg",
-  hasIcon = false,
-  inputType = "text",
+    children,
+    inputID = "default",
+    iconSrc = "icons/person.svg",
+    hasIcon = false,
+    inputType = "text",
 }) {
-  const input = useRef(null);
-  const placeholder = useRef(null);
+    const input = useRef(null);
+    const placeholder = useRef(null);
 
-  function animatedPlaceholder(event) {
-    if (document.activeElement === input.current || event.target.value !== "") {
-      placeholder.current.classList.add("input-text-placeholder-up");
-      return;
+    function animatedPlaceholder(event) {
+        if (
+            document.activeElement === input.current ||
+            event.target.value !== ""
+        ) {
+            placeholder.current.classList.add("input-text-placeholder-up");
+            return;
+        }
+        placeholder.current.classList.remove("input-text-placeholder-up");
     }
-    placeholder.current.classList.remove("input-text-placeholder-up");
-  }
 
-  return (
-    <>
-      <div
-        className="input-text-container"
-      >
-        {hasIcon ? (
-          <img className="input-text-icon" src={iconSrc} onClick={() => input.current.focus()} />
-        ) : (
-          ""
-        )}
-        <div
-          ref={placeholder}
-          className={`input-text-placeholder${hasIcon ? " placeholder-with-icon" : ""}`}
-          onClick={() => input.current.focus()}
-        >
-          {children}
-        </div>
-        <input
-          ref={input}
-          className={`input-text${hasIcon ? classes["with-icon"] : ""}`}
-          type={inputType}
-          id={inputID}
-          name={inputID}
-          onFocus={animatedPlaceholder}
-          onBlur={animatedPlaceholder}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="input-text-container">
+                {hasIcon ? (
+                    <img
+                        className="input-text-icon"
+                        src={iconSrc}
+                        onClick={() => input.current.focus()}
+                    />
+                ) : (
+                    ""
+                )}
+                <div
+                    ref={placeholder}
+                    className={`input-text-placeholder${
+                        hasIcon ? " placeholder-with-icon" : ""
+                    }`}
+                    onClick={() => input.current.focus()}
+                >
+                    {children}
+                </div>
+                <input
+                    ref={input}
+                    className={`input-text${
+                        hasIcon ? " with-icon" : ""
+                    }`}
+                    type={inputType}
+                    id={inputID}
+                    name={inputID}
+                    onFocus={animatedPlaceholder}
+                    onBlur={animatedPlaceholder}
+                />
+            </div>
+        </>
+    );
 }
 
 export default Input;
